b38c3d32167f78829cda4bcf1ebaa0958611dea3
Tridip Chakraborty <v-tchakraborty@expediagroup.com>
2019-02-02 14:02:50 +0530

    adding

---
+++ Documents/workspace-sts-3.8.3.RELEASE/StandalonePrograms/src/StringAnagramMaking.java
@@ -0,0 +1,16 @@
+
+public class StringAnagramMaking {
+	
+	static int makeAnagram(String a, String b){
+		
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
